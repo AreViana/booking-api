eab3394dc35d11cadf1c39e1604fb59f1f76952d
Arely Viana <areviana17@gmail.com>
2020-11-06 03:13:46 -0600

    🔒 add authentication strategy

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,23 @@
 class ApplicationController < ActionController::API
   include ExceptionHandler
+  include ActionController::HttpAuthentication::Token::ControllerMethods
+
+  before_action :authenticate
+
+    private
+
+    def authenticate
+      authenticate_or_request_with_http_token do |token, _options|
+        user = User.find_by(token: token)
+        raise ActionController::BadRequest.new('Invalid token') unless user
+
+        # To prevent timing attacks
+        ActiveSupport::SecurityUtils.secure_compare(token, user.token)
+        user
+      end
+    end
+
+    def current_user
+      @current_user ||= authenticate
+    end
 end
