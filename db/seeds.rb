f5566ba89e9185e2bae6c71286f6092568b2aa45
Arely Viana <areviana17@gmail.com>
2020-11-08 03:58:14 -0600

    🌱 add seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,49 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+admin = User.create(
+  email: 'are@example.com',
+  name: 'Arely Viana',
+  password: 'are_vn',
+  role: 'admin'
+)
+
+user = User.create(
+  email: 'fer@example.com',
+  name: 'Fernando',
+  password: 'fernny',
+  role: 'user'
+)
+
+activity = Activity.create(
+  name: 'Dance class',
+  kind: 'indoor'
+)
+
+Activity.create(
+  name: 'Yoga class',
+  kind: 'outdoor'
+)
+
+Activity.create(
+  name: 'Cooking class',
+  kind: 'online'
+)
+
+schedule = activity.schedules.create(
+  place: 'Sivar',
+  date: DateTime.tomorrow,
+  owner_id: admin.id
+)
+
+schedule.bookings.create(
+  user: user,
+  ticket: SecureRandom.hex(10)
+)
+
+puts 'The following records were created:'
+puts "#{User.count} users"
+puts "#{Activity.count} activities"
+puts "#{Schedule.count} schedule"
+puts "#{Booking.count} booking"
